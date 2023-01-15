665d2d4c6e3ae6e4d0ee211cd85523cfc8acebbf
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-01-15 16:32:44 -0300

    feat: created validation for course title

--- section_03/models.py
+++ section_03/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Optional
 
 
@@ -7,6 +7,17 @@ class Course(BaseModel):
     title: str
     lessons: int
     hours: int
+
+    @validator('title')
+    def validate_title(cls, value: str):
+        words = value.split(' ')
+        if len(words) < 3:
+            raise ValueError('the title must have at least 3 words')
+        if value.islower():
+            raise ValueError('the title must have the first letter capitalized')
+            
+        return value
+
 
 
 courses = [
