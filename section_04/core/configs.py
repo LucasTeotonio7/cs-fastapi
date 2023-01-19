fd5a9b87b11f75a4f7afab57da650bddd483b600
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-01-19 20:16:37 -0300

    feat: adjusted files to create database tables

--- section_04/core/configs.py
+++ section_04/core/configs.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     """
 
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://geek:university@localhost:5432/faculty'
+    DB_URL: str = 'postgresql+asyncpg://postgres:admin@localhost:5432/faculty'
     DBBaseModel = declarative_base()
 
 
