e642f0bfb6c1846256cfe2ac123cf6a430361ba4
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-02-08 20:44:50 -0300

    fix: fixed route bug

--- section_06/api/v1/api.py
+++ section_06/api/v1/api.py
@@ -6,5 +6,5 @@ from api.v1.endpoints import article, user
 api_router = APIRouter()
 
 
-api_router.include_router(article.router, prefix='articles', tags=['articles'])
-api_router.include_router(article.user, prefix='users', tags=['users'])
+api_router.include_router(article.router, prefix='/articles', tags=['articles'])
+api_router.include_router(user.router, prefix='/users', tags=['users'])
