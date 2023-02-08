cdba8455fc95456a47f11b7868cf844f5ffa458c
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-02-08 20:46:24 -0300

    fix: fixed bug in article schema

--- section_06/schemas/article_schema.py
+++ section_06/schemas/article_schema.py
@@ -3,11 +3,11 @@ from pydantic import BaseModel, HttpUrl
 
 
 class ArticleSchema(BaseModel):
-    id = Optional[int] = None
+    id: Optional[int] = None
     title: str
     description: str
     font_url: HttpUrl
-    user_id = Optional[int]
+    user_id: Optional[int]
 
     class Config:
         orm_mode=True
